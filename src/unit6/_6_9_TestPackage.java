47210666f0275b83455f0369b0f95aff1c28ee40
刘柱 <lzworker@outlook.com>
2018-06-21 01:21:31 +0800

    7.1组合语法
    懒加载模式，其中最后输出的lazy方法和打印有一些问题

--- src/unit6/_6_9_TestPackage.java
+++ src/unit6/_6_9_TestPackage.java
@@ -1,5 +1,8 @@
 package unit6;
 
-public class _6_9_TestPackage {
 
+public class _6_9_TestPackage {
+	public static void main(String[] args) {
+	
+	}
 }
---
+++ src/unit7/_7_1_SimpleLazyInitialization.java
@@ -0,0 +1,74 @@
+package unit7;
+
+/**
+ * 创建一个简单的类。第二个类中，将一个引用定义为第一个类的对象。运用惰性初始化来实例化 这个对象
+ * 
+ * 所谓惰性初始化，也就是懒加载模式，在对象使用之前才对对象进行初始化操作
+ * 
+ * @author Administrator
+ *
+ */
+class Simple {
+	String s;
+
+	public String toString() {
+		return s;
+	}
+
+	public void setString(String sNew) {
+		s = sNew;
+	}
+}
+
+// 第二个是组合：在新的类中产生现有类的对象
+class Second {
+	Simple simple;
+	String s;
+
+	public Second(String si) {
+		s = si;
+	}
+
+	// 检查第一个类Simple是否已经创建对象
+	public void checked() {
+		if (simple == null) {
+			System.out.println("simple not initialized");
+		} else {
+			System.out.println("simple initialized");
+		}
+	}
+
+	// 懒加载
+	private Simple lazy() {
+		if (simple == null) {
+			System.out.println("Creating Simple");
+			simple = new Simple();
+		}
+		return simple;
+	}
+
+	public Simple getSimple() {
+		return simple;
+	}
+
+	public void setSimple(String sNew) {
+		lazy().setString(sNew);
+	}
+
+	public String toString() {
+		return lazy().toString();
+	}
+}
+
+public class _7_1_SimpleLazyInitialization {
+	public static void main(String[] args) {
+		Second second = new Second("Init String");
+		second.checked(); // simple not initialized
+		System.out.println(second.getSimple()); // null
+		second.checked(); // simple not initialized
+		System.out.println(second.getSimple()); // null
+//		System.out.println(second); // 
+		second.setSimple("New String"); //Creating Simple; 
+		System.out.println(second); // New String
+	}
+}
