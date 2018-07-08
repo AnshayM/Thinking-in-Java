d92513f76e274858a0f8e8e2ce55ac2d98d90ac0
刘柱 <lzworker@outlook.com>
2018-07-09 00:28:43 +0800

    8.3.3构造器内部的多态方法的行为
    更新解释

--- src/unit8/PolyConstructor.java
+++ src/unit8/PolyConstructor.java
@@ -1,10 +1,10 @@
 package unit8;
 
-class  Glyph{
-	void draw(){
+class Glyph {
+	void draw() {
 		System.out.println("Glyph.draw()");
 	}
-	
+
 	public Glyph() {
 		// TODO Auto-generated constructor stub
 		System.out.println("Glyph() before draw()");
@@ -13,20 +13,31 @@ class  Glyph{
 	}
 }
 
-class RoundGlyph extends Glyph{
-	private int radius=1;
+/*
+ * 首先调用基类的构造器，但是基类中又调用了子类已经覆写过的方法，这就造成基类调用的方法，其实是子类中的方法
+ */
+class RoundGlyph extends Glyph {
+	private int radius = 1;
+
 	public RoundGlyph(int r) {
 		// TODO Auto-generated constructor stub
-		radius=r;
-		System.out.println("RoundGlyph.RoundGlphy().radius="+radius);
+		radius = r;
+		System.out.println("RoundGlyph.RoundGlphy().radius=" + radius);
 	}
-	
-	void draw(){
-		System.out.println("GroundGlyph.draw().radius="+radius);
+
+	void draw() {
+		System.out.println("GroundGlyph.draw().radius=" + radius);
 	}
 }
+
 public class PolyConstructor {
 	public static void main(String[] args) {
 		new RoundGlyph(5);
 	}
 }
+
+/*
+ * Glyph() before draw() GroundGlyph.draw().radius=0 Glyph() after draw()
+ * RoundGlyph.RoundGlphy().radius=5
+ * 
+ */
